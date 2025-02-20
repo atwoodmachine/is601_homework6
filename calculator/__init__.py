372af1ddb823daa11f765540234fd98412e45d3a
Seniz <so9@njit.edu>
2025-02-19 22:11:09 -0500

    Added new commands to calculator init

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,8 @@
 from calculator.commands import CommandHandler
 from calculator.commands.add import AddCommand
+from calculator.commands.subtract import SubtractCommand
+from calculator.commands.multiply import MultiplyCommand
+from calculator.commands.divide import DivideCommand
 from calculator.commands.exit import ExitCommand
 
 class Calculator:
@@ -8,6 +11,9 @@ class Calculator:
 
     def start(self):
         self.command_handler.register_command("add", AddCommand)
+        self.command_handler.register_command("subtract", SubtractCommand)
+        self.command_handler.register_command("multiply", MultiplyCommand)
+        self.command_handler.register_command("divide", DivideCommand)
         self.command_handler.register_command("exit", ExitCommand)
 
         print("Calculator initialized\n")
